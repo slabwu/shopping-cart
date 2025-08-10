dbdff28f0c3b1eb5eb80ce2ab34580e7e429dfeb
slabwu <134193743+slabwu@users.noreply.github.com>
2025-08-10 13:57:07 +0800

    Add homepage

---
+++ src/homepage.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div>
+      <h2>This is the homepage.</h2>
+    </div>
+  );
+};
+
+export default Homepage;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,16 @@
 import App from './App'
+import Homepage from './homepage'
 
 const routes = [
     {
         path: '/',
-        element: <App />
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Homepage />
+            },
+        ]
     }
 ]
 
