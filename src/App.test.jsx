ee4c9be66b45b30b2c6e2b12e296b3f0cf150292
slabwu <134193743+slabwu@users.noreply.github.com>
2025-10-24 16:36:00 +0800

    Create homepage tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,30 @@
-import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, it, expect, beforeEach, afterEach } from 'vitest';
+import { render, screen, cleanup } from '@testing-library/react';
+import { MemoryRouter } from "react-router-dom"
 import App from './App';
+
+beforeEach(() => {
+  render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+});
+
+afterEach(() => {
+  cleanup();
+})
 
 describe('App component', () => {
   it('renders correct heading', () => {
-    render(<App />);
-    expect(screen.getByRole('h1').textContent).toMatch(/heading/i);
+    expect(screen.getByRole('heading').textContent).toMatch(/header/i);
   });
+
+  it('renders three links', () => {
+    expect(screen.getAllByRole('link')).toHaveLength(3);
+  })
+
+  it('renders homepage'), () => {
+    expect(screen.getByText('This is the homepage.')).toBeInTheDocument();
+  } 
 });
