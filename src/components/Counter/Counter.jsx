be14a0101381e7b15b6fffbf06aa28f891d41aa9
slabwu <134193743+slabwu@users.noreply.github.com>
2025-11-27 14:49:02 +0800

    Add Counter component

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,19 @@
+import styles from './Counter.module.css'
+
+export default function Counter({ count, setCount }) {
+    function decreaseCount() {
+        if (count > 1) setCount(count => count - 1)
+    }
+
+    function increaseCount() {
+        setCount(count => count + 1)
+    }
+
+    return (
+        <div className={styles.counter}>
+            <button onClick={decreaseCount}>-</button>
+            <input type='number' value={count} onChange={e => setCount(Number(e.target.value))} />
+            <button onClick={increaseCount}>+</button>
+        </div>
+    )
+}
--- src/routes/productpage.jsx
+++ src/routes/productpage.jsx
@@ -1,8 +1,10 @@
 import { Link, useParams } from 'react-router-dom'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { ShopContext } from '../App.jsx'
+import Counter from '../components/Counter/Counter.jsx'
 
 export default function ProductPage() {
+  const [ count, setCount ] = useState(1)
   const { products, addToCart }  = useContext(ShopContext)
   const { id } = useParams()
   const product = products[id - 1]
@@ -16,6 +18,7 @@ export default function ProductPage() {
       <h3>{product.rating.rate} stars from {product.rating.count} people</h3>
       <img src={product.image} alt={product.title} />
       <div>
+        <Counter count={count} setCount={setCount} />
         <Link onClick={() => addToCart(product)} to='/cart'>Buy now</Link>
         <button onClick={() => addToCart(product)}>Add to Cart</button>
       </div>
